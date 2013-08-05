b774b48cdb86183cf218ed57379bce088e7038e9
Bogdan Solomykin <bogdan.solomykin@gmail.com>
2013-08-06 00:38:18 +0300

    initial commit

---
+++ vk/api.go
@@ -0,0 +1,53 @@
+package vk
+
+import (
+    "net/url"
+    "net/http"
+    "io/ioutil"
+    "strings"
+)
+
+const API_METHOD_URL = "https://api.vk.com/method/"
+
+type Api struct {
+	AccessToken string
+	UserId      string
+	ExpiresIn   string
+}
+
+func ParseResponseUrl(responseUrl string) (string, string, string) {
+    u, err := url.Parse(strings.Replace(responseUrl, "#", "?", 1))
+    if err != nil {
+    	panic(err)
+    }
+    
+    q := u.Query()    
+	return q.Get("access_token"), q.Get("user_id"), q.Get("expires_in")        
+}
+
+func (vk Api) Request(methodName string, params map[string]string) string {
+    u, err := url.Parse(API_METHOD_URL+methodName)
+    if err != nil {
+    	panic(err)
+    }
+
+    q := u.Query()
+    for k, v := range params {
+    	q.Set(k, v)
+    }
+    q.Set("access_token", vk.AccessToken)
+    u.RawQuery = q.Encode()
+    
+    resp, err := http.Get(u.String())
+    if err != nil {
+        panic(err)
+    }
+
+    defer resp.Body.Close()
+    content, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+    	panic(err)
+    }
+
+    return string(content)
+}
---
+++ vk/auth.go
@@ -0,0 +1,29 @@
+package vk
+
+import "net/url"
+
+const AUTH_HOST = "https://oauth.vk.com/authorize"
+
+type Auth struct {
+	AppId        string
+	Scope        string
+	RedirectUri  string
+	ResponseType string 
+}
+
+func (a Auth) GetAuthUrl() string {
+	u, err := url.Parse(AUTH_HOST)
+	if err != nil {
+        panic(err)
+	}
+
+	q := u.Query()
+	q.Set("client_id", a.AppId)
+	q.Set("scope", a.Scope)
+	q.Set("redirect_uri", a.RedirectUri)
+	q.Set("response_type", a.ResponseType)
+	u.RawQuery = q.Encode()
+
+    return u.String()
+}
+
